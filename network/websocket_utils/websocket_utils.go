fc98f8ce99d9f8ba2c30e44f9216e83db0546f30
Yui100901 <973800960@qq.com>
2025-02-18 09:49:05 +0800

    修改websocket封装增加一些描述

--- network/websocket_utils/websocket_utils.go
+++ network/websocket_utils/websocket_utils.go
@@ -12,19 +12,22 @@ import (
 // @Date 2025/1/16 09 56
 //
 
+// WSServer 是一个全局的 WebSocket 升级器
 var WSServer = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		//验证
+		// 验证
 		return true
 	},
 }
 
+// WebSocket 表示一个 WebSocket 连接
 type WebSocket struct {
-	Conn *websocket.Conn //底层websocket连接对象
+	Conn *websocket.Conn // 底层 WebSocket 连接对象
 	Done chan struct{}
 	mu   sync.Mutex
 }
 
+// NewWebSocketByUpgrade 通过 HTTP 请求升级为 WebSocket
 func NewWebSocketByUpgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*WebSocket, error) {
 	conn, err := WSServer.Upgrade(w, r, responseHeader)
 	if err != nil {
@@ -37,7 +40,8 @@ func NewWebSocketByUpgrade(w http.ResponseWriter, r *http.Request, responseHeade
 	}, nil
 }
 
-func NewWebSocketByDail(url string, requestHeader http.Header) (*WebSocket, error) {
+// NewWebSocketByDial 通过 WebSocket 地址进行连接
+func NewWebSocketByDial(url string, requestHeader http.Header) (*WebSocket, error) {
 	conn, _, err := websocket.DefaultDialer.Dial(url, requestHeader)
 	if err != nil {
 		return nil, err
@@ -49,28 +53,30 @@ func NewWebSocketByDail(url string, requestHeader http.Header) (*WebSocket, erro
 	}, nil
 }
 
+// OnMessage 注册一个处理消息的回调函数
 func (ws *WebSocket) OnMessage(handler func([]byte)) {
 	defer close(ws.Done)
 	for {
 		_, message, err := ws.Conn.ReadMessage()
 		if err != nil {
-			log_utils.Error.Println("Websocket Read ERROR:", err)
+			log_utils.Error.Println("WebSocket Read ERROR:", err)
 			return
 		}
 		if handler != nil {
 			handler(message)
 		}
-		log_utils.Info.Printf("Websocket Receive: %s", message)
+		log_utils.Info.Printf("WebSocket Receive: %s", message)
 	}
 }
 
+// SendMessage 发送一条消息
 func (ws *WebSocket) SendMessage(data func() []byte) error {
 	ws.mu.Lock()
-	err := ws.Conn.WriteMessage(websocket.TextMessage, data())
-	ws.mu.Unlock()
-	return err
+	defer ws.mu.Unlock()
+	return ws.Conn.WriteMessage(websocket.TextMessage, data())
 }
 
+// Close 关闭 WebSocket 连接
 func (ws *WebSocket) Close() {
 	ws.mu.Lock()
 	defer ws.mu.Unlock()
